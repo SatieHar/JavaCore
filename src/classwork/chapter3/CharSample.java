f60c3416bb77d99208a12b53b106aa619b04aa21
Satenik Harutyunyan <satenikharutyunian@gmail.com>
2023-08-25 23:23:16 +0400

    homework1 (updated)

--- src/classwork/chapter3/CharSample.java
+++ src/classwork/chapter3/CharSample.java
@@ -11,5 +11,8 @@ public class CharSample {
         ch1 = 89;
         ch2 = 90;
         System.out.println(ch1 + ch2);
+
+        char a = '%';
+        System.out.println(" here a is: " + a);
     }
-}
+    }
