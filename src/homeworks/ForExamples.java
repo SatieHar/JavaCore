5b0bb37f17d330941fa6ca96bfc5cdb872472904
Satenik Harutyunyan <satenikharutyunian@gmail.com>
2023-09-01 00:09:23 +0400

    homework (new)

--- src/homeworks/ForExamples.java
+++ src/homeworks/ForExamples.java
@@ -1,4 +1,11 @@
 package homeworks;
 
 public class ForExamples {
-}
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(" - ");
+            System.out.println(i + 1);
+
+            }
+        }
+    }
