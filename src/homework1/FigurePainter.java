741d5c71f100fcd7dca01ecdb61f09e7a7e17957
Satenik Harutyunyan <satenikharutyunian@gmail.com>
2023-08-31 23:49:25 +0400

    homework (updated last version)

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -2,55 +2,68 @@ package homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-        for (int a = 0; a < 1; a++)
-            for (int b = 0; b <= a; b++) {
-                System.out.println("*");
-                System.out.println("* *");
-                System.out.println("* * *");
-                System.out.println("* * * *");
-                System.out.println("* * * * *");
-                System.out.println();
+
+        System.out.println("Figure 1");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
             }
-        for (int a = 0; a < 1; a++)
-            for (int b = 0; b <= a; b++) {
-                System.out.println("* * * * *");
-                System.out.println("* * * *");
-                System.out.println("* * *");
-                System.out.println("* *");
-                System.out.println("*");
-                System.out.println();
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Figure 2");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
             }
-        for (int a = 0; a < 1; a++)
-            for (int b = 0; b <= a; b++) {
-                System.out.println(" " + " " + " " + " " + " " + " " + " " + " " + " * ");
-                System.out.println(" " + " " + " " + " " + " " + " " + " * * ");
-                System.out.println(" " + " " + " " + " " + " * * * ");
-                System.out.println(" " + " " + " * * * * ");
-                System.out.println(" * * * * * ");
-                System.out.println();
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Figure 3");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print("  ");
             }
-        for (int a = 0; a < 1; a++)
-            for (int b = 0; b <= a; b++) {
-                System.out.println(" * * * * * ");
-                System.out.println(" " + " " + " * * * * ");
-                System.out.println(" " + " " + " " + " " + " * * * ");
-                System.out.println(" " + " " + " " + " " + " " + " " + " * * ");
-                System.out.println(" " + " " + " " + " " + " " + " " + " " + " " + " * ");
-                System.out.println();
+            for (int j = 0; j <= (5 - i); j++) {
+                System.out.print("* ");
             }
-        for (int a = 0; a < 1; a++)
-            for (int b = 0; b <= a; b++) {
-                System.out.println(" " + " " + " " + " " + " * ");
-                System.out.println(" " + " " + " " + " * * ");
-                System.out.println(" " + " " + " * * * ");
-                System.out.println(" " + " * * * * ");
-                System.out.println(" * * * * * ");
-                System.out.println(" * * * * * ");
-                System.out.println(" " + " * * * * ");
-                System.out.println(" " + " " + " * * * ");
-                System.out.println(" " + " " + " " + " * * ");
-                System.out.println(" " + " " + " " + " " + " * ");
-                System.out.println();
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Figure 4");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print("  ");
             }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Figure 5");
+    for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= (5 - i); j++) {
+                System.out.print("  * ");
+            }
+            System.out.println();
+        }
+        for (int i = 4; i > 0; i--) {
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("  * ");
+            }
+            System.out.println();
+        }
     }
 }
+
