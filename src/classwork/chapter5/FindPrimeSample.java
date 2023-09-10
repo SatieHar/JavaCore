c5a034ccf4116c7d7d89993f61a0dfb20fabcf85
Satenik Harutyunyan <satenikharutyunian@gmail.com>
2023-09-10 22:41:33 +0400

    samples (2)

---
+++ src/classwork/chapter5/FindPrimeSample.java
@@ -0,0 +1,20 @@
+package classwork.chapter5;
+public class FindPrimeSample {
+    public static void main(String[] args) { //проверка на простые числа
+
+        int num;
+        boolean isPrime;
+        num = 14;
+        if (num < 2) isPrime = false;
+        else isPrime = true;
+
+        for (int i = 2; i <= num / i; i++) { // если число делится на 2, то считается простым,
+            if ((num % i) == 0) {             // если написать !=0, то получим нечетное число
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) System.out.println("Prime");
+        else System.out.println(" \nNot Prime ");
+    }
+}
---
+++ src/classwork/chapter5/ForTickSample.java
@@ -0,0 +1,14 @@
+package classwork.chapter5;
+
+public class ForTickSample {
+    public static void main(String[] args) {
+        int n;
+        for ( n = 10; n > 0 ; n--) {
+            System.out.println(" tick: " + n);
+        }
+        // or in is included in sout
+        //for (int n = 10; n > 0 ; n --) {
+        //System.out.println(" tick: " + n);
+        }
+    }
+
